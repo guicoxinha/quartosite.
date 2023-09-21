6f07b9d26de4550003005e4a0c10451546816a58
guicoxinha <145709496+guicoxinha@users.noreply.github.com>
2023-09-21 16:29:12 -0300

    meu quarto site

---
+++ main.js
@@ -0,0 +1,12 @@
+var imagens =["1.jpg 2.jpg 3.jpg 4.jpg"]
+
+var frases =["As árvores fornecem oxigêno", 
+"As árvores fornecem matéria prima",
+ "As árvores fornecem frutos",
+  "As árvores fornecem sombras"];
+  function proximo (){
+var i = 0;
+i++;
+document.getElementById("fotos").src =imagens[i];
+document.getElementById.innerHTML=frases[i];
+  }
