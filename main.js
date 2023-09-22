0058a4bcb6bbe3317730d3beac53f568cf7af371
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2023-09-21 21:40:04 -0300

    Site árvore corrigido

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-var imagens =["1.jpg 2.jpg 3.jpg 4.jpg"]
+var imagens = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
-var frases =["As árvores fornecem oxigêno", 
-"As árvores fornecem matéria prima",
- "As árvores fornecem frutos",
+var frases = ["As árvores fornecem oxigêno",
+  "As árvores fornecem matéria prima",
+  "As árvores fornecem frutos",
   "As árvores fornecem sombras"];
-  function proximo (){
-var i = 0;
-i++;
-document.getElementById("fotos").src =imagens[i];
-document.getElementById.innerHTML=frases[i];
+
+var i = 1;
+
+function proximo() {
+  document.getElementById("fotos").src = imagens[i];
+  document.getElementById("imagem").innerHTML = frases[i];
+  i++;
+
+  if(i>3){
+    i=0;
   }
+}
